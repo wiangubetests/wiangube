83d879230f2dde5547236a27fc4f31f21f5b83a1
wiangube <wiangube@gmail.com>
2019-06-22 13:48:37 -0400

    Added comments to Vital functions in Model

--- src/main/java/com/wiangube/vitals/Vital.java
+++ src/main/java/com/wiangube/vitals/Vital.java
@@ -5,26 +5,32 @@ public class Vital {
 	private boolean clean;
 	private boolean hungry;
 	
+	//Find out if William Antonio is breathing or not
 	public boolean getBreathing() {
 		return this.breathing;
 	}
 
+	//Help William Antonio to breath again
 	public void setBreathing(boolean breathing) {
 		this.breathing = breathing;
 	}
 	
+	//Find out if William Antonio is clean or not
 	public boolean getClean() {
 		return this.clean;
 	}
 
+	//Help William Antonio to stay clean again
 	public void setClean(boolean clean) {
 		this.clean = clean;
 	}
 	
+	//Find out if William Antonio is hungry or not
 	public boolean getHungry() {
 		return this.hungry;
 	}
 
+	//Help William Antonio to eat something
 	public void setHungry(boolean hungry) {
 		this.hungry = hungry;
 	}
